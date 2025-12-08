baa6b96d20e29d549d846ca182d0912c5d750bfd
Adedeji Adebayo <181992936+EM-ade@users.noreply.github.com>
2025-12-08 17:38:03 +0100

    fixed base url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,6 @@
-const API_BASE = "http://localhost:3001/api";
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL 
+  ? `${process.env.NEXT_PUBLIC_API_BASE_URL}/api`
+  : "https://vesting-be.onrender.com/api";
 // changed some things
 
 interface RequestOptions extends RequestInit {
